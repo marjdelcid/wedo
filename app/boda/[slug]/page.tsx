eb017790c9b52f1116725a4b13d52446a3016f7d
Marjorie <marjorie@485.company>
2026-04-22 18:31:41 -0600

    fix params slug

--- app/boda/[slug]/page.tsx
+++ app/boda/[slug]/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { use } from "react";
 import BodaClient from "./BodaClient";
 
-export default function BodaPage({ params }: { params: { slug: string } }) {
-  return <BodaClient slug={params.slug} />;
+export default function BodaPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = use(params);
+  return <BodaClient slug={slug} />;
 }
